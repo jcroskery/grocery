7e219597d43d1bf16519ec25fd2ed4e0cb5ef8b6
Justus Croskery <justus.croskery@gmail.com>
2022-11-06 13:58:07 +0000

    Commit more pictures

--- root/cart.js
+++ root/cart.js
@@ -17,33 +17,35 @@ function getXHR(name, post) {
 let requestData = {
     session_id: document.cookie
 }
-let xhr = getXHR('optimize', JSON.stringify(requestData))
-xhr.onload = () => {
-    let json = JSON.parse(xhr.response);
+//let xhr = getXHR('optimize', JSON.stringify(requestData))
+//xhr.onload = () => {
+//    loaded(JSON.parse(xhr.response));
+//};
+function loaded (json) {
     let line_break = document.createElement("br");
     let winnerSpan = document.createElement("span");
     winnerSpan.id = 'winner';
     winnerSpan.innerText = "Lowest Price";
-    document.getElementById(json.winner).appendChild(line_break);
-    document.getElementById(json.winner).appendChild(winnerSpan);
+    document.getElementById(json.shopping_list.winner).appendChild(line_break);
+    document.getElementById(json.shopping_list.winner).appendChild(winnerSpan);
     let nofrillsprice = 0;
-    for (obj in json.shoppinglist.zero) {
-        nofrillsprice += obj.price
+    for (obj in json.shopping_list.zero) {
+        nofrillsprice += json.shopping_list.zero[obj].price
     }
-    document.getElementById("nofrills-price").innerText = "$" + nofrillsprice;
+    document.getElementById("nofrills-price").innerText = "$" + nofrillsprice.toFixed(2);
     let loblawsprice = 0;
-    for (obj in json.shoppinglist.one) {
-        loblawsprice += obj.price
+    for (obj in json.shopping_list.one) {
+        loblawsprice += json.shopping_list.one[obj].price
     }
-    document.getElementById("loblaws-price").innerText = "$" + loblawsprice;
+    document.getElementById("loblaws-price").innerText = "$" + loblawsprice.toFixed(2);
     let metroprice = 0;
-    for (obj in json.shoppinglist.two) {
-        metroprice += obj.price
+    for (obj in json.shopping_list.two) {
+        metroprice += json.shopping_list.two[obj].price
     }
-    document.getElementById("metro-price").innerText = "$" + metroprice;
-    row.push(json.shoppinglist.zero);
-    row.push(json.shoppinglist.one);
-    row.push(json.shoppinglist.two);
+    document.getElementById("metro-price").innerText = "$" + metroprice.toFixed(2);
+    AppData.row.push(json.shopping_list.zero);
+    AppData.row.push(json.shopping_list.one);
+    AppData.row.push(json.shopping_list.two);
 }
 function goBack() {
     if (AppData.grey_div != null) {
@@ -52,36 +54,49 @@ function goBack() {
     }
 }
 function displayList(n) {
+    let image;
+    if (n == 0) {
+        image = '<img id="listimage" src="images/nofrills.png" width="200px"></img>'
+    } else if (n == 1) {
+        image = '<img id="listimage" src="images/loblaws.png" width="200px"></img>'
+    } else {
+        image = '<img id="listimage" src="images/metro.png" width="200px"></img>'
+    }
     let grey_div = document.createElement("div");
     grey_div.id = 'grey_div';
     document.getElementById('body').append(grey_div);
     let row = AppData.row[n];
     let back_button = document.createElement("button");
     back_button.id = 'back_button';
-    back_button.innerText = "back";
+    back_button.innerHTML = "<img src='images/back.png' width='40px'>";
     back_button.addEventListener('click', () => {
         goBack();
     })
+    let grid_div = document.createElement("div");
+    grid_div.id = "grid_div";
+    grid_div.innerHTML = image;
     let name_header_span = document.createElement("span");
     name_header_span.id = "name_header_span";
+    name_header_span.innerText = "Product Name";
     let price_header_span = document.createElement("span");
     price_header_span.id = "price_header_span";
+    price_header_span.innerText = "Price";
     grey_div.append(back_button);
-    grey_div.append(name_header_span);
-    grey_div.append(price_header_span);
+    grid_div.append(name_header_span);
+    grid_div.append(price_header_span);
     for (obj in row) {
         let name_span = document.createElement("span");
         name_span.classList = "name_span";
-        name_span.innerText = obj.name;
+        name_span.innerText = row[obj].name;
         let price_span = document.createElement("span");
         price_span.classList = "price_span";
-        price_span.innerText = '$' + obj.price;
-        grey_div.append(name_span);
-        grey_div.append(price_span);
+        price_span.innerText = '$' + row[obj].price;
+        grid_div.append(name_span);
+        grid_div.append(price_span);
     }
+    grey_div.append(grid_div);
     AppData.grey_div = grey_div;
 }
-displayList(0);
 for (i in [0, 1, 2]) {
     document.getElementById(i).addEventListener('click', () => {
         if (AppData.row.length > i) {
@@ -90,3 +105,8 @@ for (i in [0, 1, 2]) {
     })
 }
 
+fetch("./sample.json")
+.then(response => {
+   return response.json();
+})
+.then(data => loaded(data));
